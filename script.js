9033978a9e6c62c9266db472ebdc6f533e7c8b3a
lunakish <lunakish6@gmail.com>
2023-06-25 23:23:06 +0300

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const accessKey="6DMwTx2G455QNeD6dh4VOMN64-PfetMT9gHY0niraFM"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page =1
